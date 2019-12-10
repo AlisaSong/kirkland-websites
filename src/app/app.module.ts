0c55d23faacc362998d13e6f677059101e961ea5
Alisa Song <alisasong@gmail.com>
2019-12-09 18:30:28 -0800

    Add header, footer, and content components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { WorkwithusComponent } from './components/workwithus/workwithus.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    PortfolioComponent,
+    PricingComponent,
+    WorkwithusComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
