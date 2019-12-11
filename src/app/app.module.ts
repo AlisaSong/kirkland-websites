11c22396b7d251fbddac97b89d102050005aa4a4
Alisa Song <alisasong@gmail.com>
2019-12-10 20:17:17 -0800

    remove old components' paths, add header, add to app html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { PricingComponent } from './components/pricing/pricing.component';
-import { WorkwithusComponent } from './components/workwithus/workwithus.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContentComponent } from './components/content/content.component';
 
@@ -15,10 +11,6 @@ import { ContentComponent } from './components/content/content.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent,
-    PortfolioComponent,
-    PricingComponent,
-    WorkwithusComponent,
     FooterComponent,
     ContentComponent
   ],
