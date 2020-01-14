405bb0768a01bdb99d2443021c868eb7004afc64
Alisa Song <alisasong@gmail.com>
2020-01-13 17:24:31 -0800

    Add routing for portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContentComponent } from './components/content/content.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: PortfolioComponent,
+  path: 'portfolio/:id'
+}, {
+  component: ContentComponent,
+  path: ''
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
